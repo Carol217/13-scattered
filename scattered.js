d38b908270c558f5191131188026f27b49fd8d54
Valery Chen <cpan3@stuy.edu>
2018-03-20 21:10:48 -0400

    added axis

--- scattered.js
+++ scattered.js
@@ -7,6 +7,25 @@
 var svg = document.getElementById("svg");
 var title = document.getElementById("title");
 
+var x_axis = document.createElementNS("http://www.w3.org/2000/svg", "line");
+x_axis.setAttribute("x1", 30);
+x_axis.setAttribute("x2",1000);
+x_axis.setAttribute("y1", 370);
+x_axis.setAttribute("y2", 370);
+x_axis.setAttribute("stroke-width", 2);
+x_axis.setAttribute("stroke", "black");
+svg.appendChild(x_axis);
+
+var y_axis = document.createElementNS("http://www.w3.org/2000/svg", "line");
+y_axis.setAttribute("y1", 0);
+y_axis.setAttribute("y2", 370);
+y_axis.setAttribute("x1", 30);
+y_axis.setAttribute("x2", 30);
+y_axis.setAttribute("stroke-width", 2);
+y_axis.setAttribute("stroke", "black");
+svg.appendChild(y_axis);
+
+
 var x_vals = [9, 8, 6];//temp data
 var y_vals = [5, 8, 4];//temp data
 var data = [];
