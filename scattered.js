9a02b86377dee25756b11996b567f6b9a7467651
doniatung <doniatung32@gmail.com>
2018-03-21 22:39:13 -0400

    can't figure out labels omg

--- scattered.js
+++ scattered.js
@@ -26,12 +26,8 @@ y_axis.setAttribute("stroke", "black");
 svg.appendChild(y_axis);
 
 
-//var x_vals = [12, 12, 6];//temp data
-//var y_vals = [12, 17, 4];//temp data
-var data = [];
-
-var x_vals = [8,8.5,8.5,9.5,8.5,11,10.5,12.5,15,17.5,15,16,20]
-var y_vals = [6,6,7,7,7.5,8,9,9.5,10,11,10,10.5,11]
+var x_vals = [5,5,10.2,5,7.6,6,12,10.8,6.2,5.8,4.6,6.8,5,4.6,3.8,14.2,5.2,6.2,5.2,12.4]
+var y_vals = [4.4,6.4,10,5,6.6,5.4,9,9.4,6,8.8,4.6,7.8,4.8,4.4,3.2,14.6,5.4,7.2,4.8,12]
 
 
 for (var i = 0; i < x_vals.length; i +=1){
@@ -42,6 +38,23 @@ for (var i = 0; i < x_vals.length; i +=1){
     svg.appendChild(c);
 }
 
+var labelsX = d3.select("svg").selectAll("text").data(x_vals)
+labelsX.append("text")
+  .attr("x", function(d) { return  d[0] + 30; })
+  .attr("y", function() { return 459; })
+  .text(function(d){
+    return d[0] *5;
+  });
+
+var labelsY = d3.select("svg").selectAll("text").data(y_vals)
+labelsY.append("text")
+  .attr("x", function(d) { return 30; })
+  .attr("y", function() { return d[0] + 105; })
+  .text(function(d)
+{
+  return d[0]*5 ;
+})
+
 var plot = function(){
     var circles = d3.selectAll("circle");
     circles.data(x_vals);
@@ -51,4 +64,3 @@ var plot = function(){
 }
 
 plot();
-//add in axis later
